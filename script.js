be686cccd200b88aec44ef50bf67fc341adc63d4
Ryan Lee <rjlguitar@gmail.com>
2021-03-21 21:14:19 -0700

    changed link message

--- script.js
+++ script.js
@@ -9,24 +9,21 @@ document.getElementById('search').addEventListener('keyup', function (event) {
 
 function searchLink() {
   const amazonsearch = input.value;
-  document.getElementById('amazon').innerHTML =
-    'Here is a link to your search results';
+  document.getElementById('amazon').innerHTML = 'Open results in new tab';
   document.getElementById(
     'amazon'
   ).href = `https://www.amazon.com/s?k=${amazonsearch}&ref=nb_sb_noss_1`;
   document.getElementById('amazon').target = '_blank';
 
   const targetsearch = input.value;
-  document.getElementById('target').innerHTML =
-    'Here is a link to your search results';
+  document.getElementById('target').innerHTML = 'Open results in new tab';
   document.getElementById(
     'target'
   ).href = `https://www.target.com/s?searchTerm=${targetsearch}`;
   document.getElementById('target').target = '_blank';
 
   const walmartsearch = input.value;
-  document.getElementById('walmart').innerHTML =
-    'Here is a link to your search results';
+  document.getElementById('walmart').innerHTML = 'Open results in new tab';
   document.getElementById(
     'walmart'
   ).href = `https://www.walmart.com/search/?query=${walmartsearch}`;
